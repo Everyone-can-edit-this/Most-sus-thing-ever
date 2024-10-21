fb34448419e3b492aef1f8a52b9d1f1e569383b8
Some anyone <someanyone04@outlook.com>
2024-10-21 17:59:47 +0200

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,42 @@
+echo "welcome!!!"
+echo "--------------"
+echo "please confirm everything"
+
+sudo add-apt-repository ppa:flatpak/stable
+sudo apt update
+sudo apt install flatpak neofetch
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+neofetch
+flatpak install cointop helix Boxi
+
+neofetch
+ls -a ./
+ls -a /
+mkdir ./sus
+cd ./sus
+touch mogus.txt
+file mogus.txt
+
+neofetch
+useradd impostor
+
+echo "let's play ping pong"
+ping pong.ru
+wget -m pong.ru 
+wget -m google.com
+wget -m github.com
+wget -m bing.com
+wget -m pornhub.com
+
+echo "lets remove some bloat!"
+sudo apt remove *gnome*
+sudo apt remove *kde*
+echo "xfce is better"
+sudo apt install *xfce*
+sudo apt autoremove
+
+neofetch
+neofetch
+
